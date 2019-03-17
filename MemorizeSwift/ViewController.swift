91c9bdbb7cccd5bdd49755598ddceac85ffdfad1
ljrocha <ljrocha@users.noreply.github.com>
2019-03-16 17:51:53 -0700

    Initial commit

---
+++ MemorizeSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemorizeSwift
+//
+//  Created by Leandro Rocha on 3/16/19.
+//  Copyright © 2019 Leandro Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
