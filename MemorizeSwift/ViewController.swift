7ef9cc8071eec2fedcb9edb6d6224acc1567a07e
ljrocha <ljrocha@users.noreply.github.com>
2019-03-16 18:44:30 -0700

    Read initial list of Swift terms and display the description on the detail view controller

--- MemorizeSwift/ViewController.swift
+++ MemorizeSwift/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var swiftTerms = [SwiftTerm]()
+    
+    // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "Swift Terms"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        guard let url = Bundle.main.url(forResource: "SwiftTerms", withExtension: ".json") else {
+            fatalError("Unable to locate SwiftTerms.json")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Unable to load contents of SwiftTerms.json")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let terms = try? decoder.decode([SwiftTerm].self, from: data) else {
+            fatalError("Unable to decode SwiftTerms.json")
+        }
+        
+        swiftTerms = terms
+    }
+    
+    //MARK: - Table view data source methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return swiftTerms.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Term", for: indexPath)
+        cell.textLabel?.text = swiftTerms[indexPath.row].term
+        return cell
     }
 
+    //MARK: - Table view delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.swiftTerm = swiftTerms[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
 
